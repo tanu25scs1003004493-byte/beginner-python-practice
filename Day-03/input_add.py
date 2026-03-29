33113c6b85127d5edcc703994faef05a2e6fed1f
Tanu priya <tanu.25scs1003004493@iilm.edu>
2026-03-29 19:37:54 +0530

    Create input_add.py

---
+++ Day-03/input_add.py
@@ -0,0 +1,6 @@
+ #Taking input from user and adding two numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = num1 + num2
+print("Sum is:", result)
